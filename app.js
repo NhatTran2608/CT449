ca2e40c0f81e5b8de2e61af2a684390d41b50c8c
NhatTran2608 <112406282+NhatTran2608@users.noreply.github.com>
2023-02-13 01:27:36 +0700

    modified:   app.js 	modified:   app/controllers/contact.controllers.js 	new file:   app/router/contact.route.js 	deleted:    app/routes/contact.route.js 	modified:   package.json

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const contactsRouter = require("./app/routes/contact.route");
+
 const cors = require("cors")
+const contactsRouter = require("./app/router/contact.route");
 const ApiError = require("./app/api-error");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
-
-app.use((req, res, next) => {
-  return next(new ApiError(404, "Resource not found"));
-});
-
-app.use((err, req, res, next) => {
-  return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
-  });
-});
-
 app.get('/', (req, res) => {
-  res.json({ message: "Welcom to contact book application." });
+    res.json({ message: "Welcom to contact book application." });
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
