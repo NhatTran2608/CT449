eed91ba1757bcacf5ae69aa2f27b7ff098131e89
NhatTran2608 <112406282+NhatTran2608@users.noreply.github.com>
2023-02-13 10:40:45 +0700

    modified:   app.js

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.json({ message: "Welcom to contact book application." });
+  res.json({ message: "Welcom to contact book application." });
 });
 
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res, next) => {
 
-    return next(new ApiError(404, "Resource not found"));
+  return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
 
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 
 
